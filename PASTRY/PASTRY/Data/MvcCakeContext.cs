7d41b4cdd4f77ad179bac7b702e8ed3c7c478ff8
RBianca <biancasorinarusu@yahoo.ro>
2021-05-16 12:45:37 +0300

    Insert in database

---
+++ PASTRY/PASTRY/Data/MvcCakeContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PASTRY.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PASTRY.Data
+{
+    public class MvcCakeContext : DbContext
+    {
+        public MvcCakeContext(DbContextOptions<MvcCakeContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Cake> Cake { get; set; }
+    }
+}
