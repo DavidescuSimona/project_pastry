7f57df9f030f7eb7f0147f88cf1d1ac604756241
Melaniaift <ioanamelany99@yahoo.com>
2021-05-16 20:22:37 +0300

    Login update

--- PASTRY/PASTRY/Startup.cs
+++ PASTRY/PASTRY/Startup.cs
@@ -1,18 +1,16 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
+using PASTRY.Data;
+using Microsoft.EntityFrameworkCore;
 namespace PASTRY
 {
+    
     public class Startup
     {
+        public static bool isLogged = false;
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -24,6 +22,9 @@ namespace PASTRY
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddDbContext<MvcCakeContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("MvcCakeContext")));
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
