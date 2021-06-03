0d64b385cc2d75a8d0c2257ed33d20763c5b7244
Simona Davidescu <simonadavidescu04@gmail.com>
2021-06-04 00:49:59 +0300

    Modify Images View

--- PASTRY/PASTRY/Program.cs
+++ PASTRY/PASTRY/Program.cs
@@ -21,17 +21,7 @@ namespace PASTRY
             {
                 var services = scope.ServiceProvider;
 
-                try
-                {
-                    SeedData.Initialize(services);
-
-                    SeedDrinks.Initialize(services);
-                }
-                catch (Exception ex)
-                {
-                    var logger = services.GetRequiredService<ILogger<Program>>();
-                    logger.LogError(ex, "An error occurred seeding the DB.");
-                }
+               
                 
             }
 
