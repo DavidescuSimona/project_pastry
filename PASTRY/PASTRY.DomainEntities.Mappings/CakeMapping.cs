4428a750a25175f15e2113a9d13353278539e409
Simona Davidescu <simonadavidescu04@gmail.com>
2021-06-05 15:42:53 +0300

    Mappings

---
+++ PASTRY/PASTRY.DomainEntities.Mappings/CakeMapping.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PASTRY.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PASTRY.DomainEntities.Mappings
+{
+    class CakeMapping : IEntityTypeConfiguration<Cake>
+    {
+        public void Configure(EntityTypeBuilder<Cake> builder)
+        {
+            builder.ToTable("Cakes");
+            builder.Property(c => c.Id).HasColumnName("Id");
+        }
+    }
+}
